34bac40afa018421f74032b813abd58735a63d5f
Theo <theo.morales.fr@gmail.com>
2025-10-17 21:40:02 +0100

    ImportHook: add simple unit test

--- tests/test_import_hook.py
+++ tests/test_import_hook.py
@@ -1,13 +1,26 @@
+import os
+import sys
 import unittest
 
+from fever import Fever
 
+
+# Cases we want to handle:
+# import foo
+# from foo import bar where bar is a function
+# from foo import bar where bar is a module
+# from foo import bar where bar is a class
+# from foo import * where we want to import the foo module basically
 class TestImportHook(unittest.TestCase):
-    def test_import_hook(self):
-        # TODO: Make a little test suite :)
-        # Cases we want to handle:
-        # import foo
-        # from foo import bar where bar is a function
-        # from foo import bar where bar is a module
-        # from foo import bar where bar is a class
-        # from foo import * where we want to import the foo module basically
-        self.assertTrue(False)
+    def setUp(self):
+        self.fever = Fever()
+        self.fever.setup()
+
+    def tearDown(self):
+        self.fever.cleanup()
+
+    def test_import_hook_simple(self):
+        sys.path.append(os.path.join(os.getcwd(), "tests/test_imports"))
+        import module_a  # noqa: F401
+
+        self.assertTrue("module_a" in self.fever.dependency_tracker.all_imports)
